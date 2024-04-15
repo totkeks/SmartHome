aa0d633411ba9758d841349b0a9b76e7d731f9df
Norman Dankert <norman.dankert@outlook.com>
2024-04-15 19:37:00 +0200

    Refactoring U-Boot build files

--- devices/router/u-boot/build.sh
+++ devices/router/u-boot/build.sh
@@ -5,13 +5,7 @@ set -eu
 echo "Starting U-Boot build process..."
 
 echo "Reading additional makeflags..."
-makeflags=()
-while IFS= read -r line
-do
-	# Skip lines starting with # or empty lines
-	[[ $line =~ ^# ]] || [[ -z $line ]] && continue
-	makeflags+=("$line")
-done < makeflags.txt
+mapfile -t makeflags < <(grep -v '^#' ../makeflags.txt | grep -v '^$')
 
 cd u-boot
 
@@ -21,7 +15,7 @@ make mt7988a_bpir4_sd_defconfig
 echo "Building U-Boot..."
 make -j$(nproc) CROSS_COMPILE=aarch64-linux-gnu- "${makeflags[@]}"
 
-echo "Copying binary to the output directory..."
+echo "Copying binary to the firmware directory..."
 cp -v u-boot.bin ../firmware
 
 echo "U-Boot build process was successful."
